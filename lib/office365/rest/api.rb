ee8ed747d9437e52fcf867b5cb76ed1997b3ce5a
dale.zhang <dale.zhang@ekohe.com>
2022-10-25 11:24:11 +0800

    FEATURE - Add contact API - Add params for mailbox

--- lib/office365/rest/api.rb
+++ lib/office365/rest/api.rb
@@ -3,6 +3,7 @@
 require_relative "./user"
 require_relative "./mailbox"
 require_relative "./calendar"
+require_relative "./contact"
 require_relative "./token"
 
 module Office365
@@ -11,6 +12,7 @@ module Office365
       include Office365::REST::User
       include Office365::REST::Mailbox
       include Office365::REST::Calendar
+      include Office365::REST::Contact
       include Office365::REST::Token
     end
   end
--- lib/office365/utils.rb
+++ lib/office365/utils.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require "office365/utils/string"
+require "office365/utils/to_query"
